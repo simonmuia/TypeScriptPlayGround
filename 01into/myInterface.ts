9828b30f4666000bad9e53387fc0357f010cbf33
simonmuia <simonmuia38@gmail.com>
2023-01-19 19:04:37 +0300

    Interfaces vs Type

--- 01into/myInterface.ts
+++ 01into/myInterface.ts
@@ -7,13 +7,32 @@ interface User {
 //    Method 1
     // startTrail: () =>string
     // Method 2
-    startTrail(): string
+    startTrail(): string,
+    getCoupon(couponname: string, value:number) : number
 }
 
-const simon: User ={dbId:22, email:"example@example.com", userId: 2211,
+interface User {
+    githubToken: string
+}
+
+//Inheritance
+
+interface Admin extends User {
+    role:"admin" | "ta" | "learner"
+}
+
+const simon: Admin ={dbId:22, email:"example@example.com", userId: 2211,
+githubToken:"github",
+role: "admin",
+
 startTrail:()=>{
     return "trail started"
-}}
+},
+getCoupon: (name:"simon", off:10) => {
+    return 10
+}
+
+}
 
 simon.email = "simon@gme.com"
 // simon.dbId = 3434
