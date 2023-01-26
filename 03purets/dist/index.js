f4f74bf09fdb09d9f12c6568de4f3d94747f6be3
simonmuia <simonmuia38@gmail.com>
2023-01-26 18:31:36 +0300

    Added Classes

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,3 +1,53 @@
 "use strict";
-console.log("Typescript is here still");
-console.log("TypeScript is amazing");
+// class User {
+//  public email: string;
+//   name: string;
+//   private readonly city: string=""
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city
+//   }
+// }
+//Classes
+//Getters and setters
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        //Protected, is accessible to any child class
+        this._courseCount = 1;
+        this.city = "Nairobi";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    //gets any property(both private and public)
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    //setter doesn't have any return type
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+//Inheritance
+//Inheriting classes cannot acquire private properties of parent class
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const simon = new User("s@s.com", "Simon");
+simon.email;
+// simon.deleteToken()
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,2 +1,60 @@
-console.log("Typescript is here still");
-console.log("TypeScript is amazing");
+// class User {
+//  public email: string;
+//   name: string;
+//   private readonly city: string=""
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city
+//   }
+// }
+
+//Classes
+//Getters and setters
+class User {
+    //Protected, is accessible to any child class
+    protected _courseCount = 1
+
+    readonly city: string = "Nairobi"
+    constructor(public email: string,public name: string){
+
+    }
+
+    private deleteToken(){
+        console.log("Token deleted");
+        
+    }
+    //gets any property(both private and public)
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+
+    get courseCount():number {
+        return this._courseCount
+    }
+
+    //setter doesn't have any return type
+    set courseCount(courseNum) {
+        if (courseNum<=1) {
+            throw new Error("Course count should be more than 1");
+            
+        }
+        this._courseCount = courseNum;
+    }
+}
+
+
+//Inheritance
+//Inheriting classes cannot acquire private properties of parent class
+class SubUser extends User {
+    isFamily: boolean = true;
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const simon = new User("s@s.com", "Simon");
+simon.email
+
+// simon.deleteToken()
