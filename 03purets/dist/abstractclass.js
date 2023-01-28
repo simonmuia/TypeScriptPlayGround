34a9255699c4cbea7a9364f526427f5ce9db0a3e
Simon Muia <simonmuia38@gmail.com>
2023-01-28 08:25:49 +0300

    Abstracts and Inheritance

--- 03purets/dist/abstractclass.js
+++ 03purets/dist/abstractclass.js
@@ -4,7 +4,21 @@ class TakePhoto {
         this.cameraMode = cameraMode;
         this.filter = filter;
     }
+    getReelTime() {
+        //some complex calculation
+        return 8;
+    }
 }
 class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter); //super is used to call constructor of the parent class
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSerpia() {
+        console.log("Serpia");
+    }
 }
-// const sm = new TakePhoto("Test","test")
+const sm = new Instagram("Test", "test", 3);
+sm.getReelTime();
--- 03purets/src/abstractclass.ts
+++ 03purets/src/abstractclass.ts
@@ -1,13 +1,27 @@
-abstract class TakePhoto{
-    constructor(
-        public cameraMode: string,
-        public filter: string
-        
-        ){}
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+
+  abstract getSerpia(): void;
+  getReelTime(): number {
+    //some complex calculation
+    return 8;
+  }
 }
 
-class Instagram extends TakePhoto{
+class Instagram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter); //super is used to call constructor of the parent class
+  }
 
+  getSerpia(): void {
+    console.log("Serpia");
+  }
 }
 
-// const sm = new TakePhoto("Test","test")
+const sm = new Instagram("Test","test",3)
+
+sm.getReelTime()
